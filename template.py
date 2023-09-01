b51c5cefdd12353c5f323ec9ef33d76529ec3afb
Nicole <95356395+astronicmn@users.noreply.github.com>
2023-09-01 01:52:09 -0300

    my template

---
+++ template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+	DESCRIÇÃO:
+
+	AUTOR:
+		N. M. Narvaz
+		00318736@ufrgs.br
+	
+	REFERÊNCIAS: 
+		<[#] referência..>
+	
+	BUGS/LIMITAÇÕES:
+
+	ToDos:
+		<alterações/correções a fazer>
+	
+	OBSERVAÇÕES:
+
+	VERSÃO:
+	0.01 - <dd.mm.yyyy> 
+"""
